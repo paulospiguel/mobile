a9a98609b7aac3ecd87b5d4fb15399a8a907d163
Paulo Spiguel <paulospil@hotmail.com>
2022-11-01 12:15:41 +0000

    added logo

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,9 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Image} from "native-base";
 
 export const SignIn = () => {
   return (
     <Center bgColor="black" flex={1}>
+      <Image src="../assets/nlwCopa.svg" />
       <Text color="white" fontSize={24}>
         NLW Copa
       </Text>
